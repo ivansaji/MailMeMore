b4347051e1aae19d790df32d41b01033122e9807
Ivan Saji Abraham <ivansaji619@gmail.com>
2019-12-17 19:38:37 +0530

    thyth

--- mailmemore.py
+++ mailmemore.py
@@ -1,33 +1,24 @@
-import time
-import random
-import smtplib          #mail protocol client
-from email.mime.multipart import MIMEMultipart      #Import modules for using mail servers
 
-gmail_user = "abcd@gmail.com"   #Enter the user email ID
-gmail_pwd = "your passwd"       #Enter the User Password
-TO = ['1234@gmail.com']     # must be a list (Recepient mail)
-msg = MIMEMultipart()
-time.sleep(1)
-otp = 'Your OTP is  ' + str(random.randint(10000,99999))
-msg['Subject'] = otp
-try:
-    server = smtplib.SMTP("smtp.gmail.com",587)
-    print("SMTP Success")
-    server.ehlo()
-    print("EHLO Success")
-    server.starttls()
-    print("TLS Success")
-    server.login(gmail_user,gmail_pwd)
-    print("Login Success")
-    server.sendmail(gmail_user,TO,msg.as_string())
-    print("sending.....")
-    server.close()
-    print("successfully send mail....")
-except:
-    print("Some Error")
+def logo():
+    print('''
+ /$$      /$$           /$$ /$$       /$$      /$$                 /$$      /$$                              
+| $$$    /$$$          |__/| $$      | $$$    /$$$                | $$$    /$$$                              
+| $$$$  /$$$$  /$$$$$$  /$$| $$      | $$$$  /$$$$  /$$$$$$       | $$$$  /$$$$  /$$$$$$   /$$$$$$   /$$$$$$ 
+| $$ $$/$$ $$ |____  $$| $$| $$      | $$ $$/$$ $$ /$$__  $$      | $$ $$/$$ $$ /$$__  $$ /$$__  $$ /$$__  $$
+| $$  $$$| $$  /$$$$$$$| $$| $$      | $$  $$$| $$| $$$$$$$$      | $$  $$$| $$| $$  \ $$| $$  \__/| $$$$$$$$
+| $$\  $ | $$ /$$__  $$| $$| $$      | $$\  $ | $$| $$_____/      | $$\  $ | $$| $$  | $$| $$      | $$_____/
+| $$ \/  | $$|  $$$$$$$| $$| $$      | $$ \/  | $$|  $$$$$$$      | $$ \/  | $$|  $$$$$$/| $$      |  $$$$$$$
+|__/     |__/ \_______/|__/|__/      |__/     |__/ \_______/      |__/     |__/ \______/ |__/       \_______/
+                                                                                                                                                                                                             
+Created By: CyberFreak | Code:v1                                                                                        
+                                                                                                 
+    ''')
 
-user_otp = input("Enter your OTP send to "+ TO)
-if user_otp == otp:
-    print("OTP Accepted")
-else:
-    print("Invalid OTP")
+def main():
+    logo()
+    recepient_mail = input("\nEnter the Victim Mail to Bomb :\n")
+    attacker_mail = input("\nEnter the attacker mail \n")
+    attacker_mail_password = input("\nEnter attacker Mail Password\n")
+    opt1 = int(input("\nChoose an Option:\n\t1)Use default body\n\t2)Use Custom text\n"))
+    if(opt1 == 1):
+        message = ''
---
+++ test.py
@@ -0,0 +1,27 @@
+import time
+import random
+import smtplib          #mail protocol client
+from email.mime.multipart import MIMEMultipart      #Import modules for using mail servers
+
+gmail_user = "abcd@gmail.com"   #Enter the user email ID
+gmail_pwd = "your passwd"       #Enter the User Password
+TO = ['1234@gmail.com']     # must be a list (Recepient mail)
+msg = MIMEMultipart()
+time.sleep(1)
+otp = 'Your OTP is  ' + str(random.randint(10000,99999))
+msg['Subject'] = otp
+try:
+    server = smtplib.SMTP("smtp.gmail.com",587)
+    print("SMTP Success")
+    server.ehlo()
+    print("EHLO Success")
+    server.starttls()
+    print("TLS Success")
+    server.login(gmail_user,gmail_pwd)
+    print("Login Success")
+    server.sendmail(gmail_user,TO,msg.as_string())
+    print("sending.....")
+    server.close()
+    print("successfully send mail....")
+except:
+    print("Some Error")
